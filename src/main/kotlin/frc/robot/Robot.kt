a800566c7e60b121faef11e525381afc2ea14221
PY44N <pyan4444@gmail.com>
2022-05-07 11:38:41 -0400

    Worked on swolibb

--- src/main/kotlin/frc/robot/Robot.kt
+++ src/main/kotlin/frc/robot/Robot.kt
@@ -15,7 +15,6 @@ import frc.robot.util.FieldPosition
 import frc.robot.util.IO
 import java.util.Map
 
-
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
