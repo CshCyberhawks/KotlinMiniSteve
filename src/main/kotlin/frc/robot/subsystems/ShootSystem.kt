5787311c7793164d310ed72ceecd09427d227037
PY44N <pyan4444@gmail.com>
2022-10-14 11:58:21 -0400

    Forgot to commit this

--- src/main/kotlin/frc/robot/subsystems/ShootSystem.kt
+++ src/main/kotlin/frc/robot/subsystems/ShootSystem.kt
@@ -16,7 +16,7 @@ import frc.robot.Robot
 class ShootSystem() : SubsystemBase() {
 // traversalEncoder = traversalMotor.getEncoder();
 
-// Set distance to pulse to distance in rotation (makes get rate return
+    // Set distance to pulse to distance in rotation (makes get rate return
 // rotations)
 // Why write readable code when I can write this 1/8192
 // bottom wheel encoder -3.7 for perfect shot
@@ -24,8 +24,10 @@ class ShootSystem() : SubsystemBase() {
 // top wheel encoder 22 max
 // bottom wheel encoder -24 max
     private var topMotor: CANSparkMax = CANSparkMax(Constants.topShootMotor, CANSparkMaxLowLevel.MotorType.kBrushless)
-    private var bottomRightMotor: CANSparkMax = CANSparkMax(Constants.rightShootMotor, CANSparkMaxLowLevel.MotorType.kBrushless)
-    private var bottomLeftMotor: CANSparkMax = CANSparkMax(Constants.leftShootMotor, CANSparkMaxLowLevel.MotorType.kBrushless)
+    private var bottomRightMotor: CANSparkMax =
+        CANSparkMax(Constants.rightShootMotor, CANSparkMaxLowLevel.MotorType.kBrushless)
+    private var bottomLeftMotor: CANSparkMax =
+        CANSparkMax(Constants.leftShootMotor, CANSparkMaxLowLevel.MotorType.kBrushless)
     private var topEncoder: Encoder = Encoder(2, 3)
     private var bottomEncoder: Encoder = Encoder(0, 1)
     private var oldEncoder: RelativeEncoder = topMotor.encoder
@@ -36,7 +38,8 @@ class ShootSystem() : SubsystemBase() {
     var bottomWheelSpeed = 0.0
     var topWheelSpeed = 0.0
 
-    private var shootSpeedTable: NetworkTableEntry = Robot.driveShuffleboardTab.add("Shoot Speed", topEncoder.getRate()).getEntry()
+    private var shootSpeedTable: NetworkTableEntry =
+        Robot.driveShuffleboardTab.add("Shoot Speed", topEncoder.getRate()).getEntry()
     private var isAtSpeedTable: NetworkTableEntry = Robot.driveShuffleboardTab.add("At Desired Speed", false).getEntry()
 
     // bottom wheel encoder -3.7 for perfect shot
@@ -46,8 +49,8 @@ class ShootSystem() : SubsystemBase() {
     // bottom wheel encoder -24 max
 
     init {
-        topEncoder.setDistancePerPulse(1.0/8192.0)
-        bottomEncoder.setDistancePerPulse(1.0/8192.0)
+        topEncoder.setDistancePerPulse(1.0 / 8192.0)
+        bottomEncoder.setDistancePerPulse(1.0 / 8192.0)
     }
 
     fun getTopEncoder(): Encoder {
@@ -92,7 +95,7 @@ class ShootSystem() : SubsystemBase() {
         SmartDashboard.putNumber("Top Encoder", topEncoder.getRate())
         SmartDashboard.putNumber("Bottom Encoder", bottomEncoder.getRate())
         shootSpeedTable.setDouble(topEncoder.getRate())
-        if (shooting) {
+        if (!shooting) {
             topMotor.set(0.0);
             bottomLeftMotor.set(0.0);
             bottomRightMotor.set(0.0);
