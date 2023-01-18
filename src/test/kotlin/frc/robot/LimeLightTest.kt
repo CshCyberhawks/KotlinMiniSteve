1c74803db5abea9023c9b144f539a50e78dc89b2
Milan Lustig <mguinangreen726@gmail.com>
2023-01-18 18:34:56 -0500

    UNIT TESTS

---
+++ src/test/kotlin/frc/robot/LimeLightTest.kt
@@ -0,0 +1,28 @@
+package frc.robot.subsystems
+
+import org.junit.Test
+import frc.robot.subsystems.Limelight
+import org.junit.Assert.*
+import frc.robot.util.Vector2
+
+class LimeLightTest {
+    @Test
+    fun getPositionStraight() {
+        val limelight = Limelight(0.711, 0.24, 40.0)
+        assert(limelight.getPosition(1.0, 0.0, 0.0) == Vector2(1.0, 0.0))
+    }
+
+    @Test
+    fun getPositionCurvy() {
+        val limelight = Limelight(0.711, 0.24, 40.0)
+        assertEquals(limelight.getPosition(1.0, 5.0, 0.0).x, .996, .05)
+        assertEquals(limelight.getPosition(1.0, 5.0, 0.0).y, .087, .05)
+    }
+
+    @Test
+    fun getPositionCurvyAndAdd() {
+        val limelight = Limelight(0.711, 0.24, 40.0)
+        assertEquals(limelight.getPositionAndAddRobot(1.0, 5.0, 0.0, Vector2(1.0, 1.0)).x, 1.996, .05)
+        assertEquals(limelight.getPositionAndAddRobot(1.0, 5.0, 0.0, Vector2(1.0, 1.0)).y, 1.087, .05)
+    }
+}
